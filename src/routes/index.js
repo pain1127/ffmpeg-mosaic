939bfa561ea1617bd487703173e0902331dd06bb
pain1127 <sk.park@catenoid.net>
2021-05-25 00:20:13 +0900

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = new express.Router();
+const {exec} = require('child_process');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  exec('dir', (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  res.send('test');
 });
 
 module.exports = router;
